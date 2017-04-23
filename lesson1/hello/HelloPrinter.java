860b94fdc798d4ec97ff2e65c65e9602069b3df7
Nathan <thecookielord2000@gmail.com>
2017-04-23 20:47:54 +0000

    resolveing atempt

--- lesson1/hello/HelloPrinter.java
+++ lesson1/hello/HelloPrinter.java
@@ -5,6 +5,9 @@ public class HelloPrinter
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
-        System.out.println("testing stuff");
+        System.out.println("the cow is dead");
+        System.out.println("the cow is very dead");
+        System.out.println("the cow is tired");
     }
+
 }
